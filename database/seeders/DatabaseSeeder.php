ed88f9d90863c02f1dc4039155e17e1e03b0b7ec
sdlbdg <sidolapakbdg@gmail.com>
2021-11-18 17:13:45 +0700

    DB seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace Database\Seeders;
-
+use Laravolt\Indonesia\Seeds\CitiesSeeder;
+use Laravolt\Indonesia\Seeds\VillagesSeeder;
+use Laravolt\Indonesia\Seeds\DistrictsSeeder;
+use Laravolt\Indonesia\Seeds\ProvincesSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        $this->call(JneDestinationSeeder::class);
+        $this->call(JneOriginSeeder::class);
+
+
+
+        // $this->call([
+        //     ProvincesSeeder::class,
+        //     CitiesSeeder::class,
+        //     DistrictsSeeder::class,
+        //     VillagesSeeder::class,
+        // ]);
     }
 }
